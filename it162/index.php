35ea60dde7fd00bdf66b2f23fd022402a265f656
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-11-08 23:07:31 -0800

    it162 commit 11/08/2022
    attention: Ms. Newman
    error related to contact form or email is not appearing. contact.php and includes
    Also I have two folders name the same includes mistake and includes. These two folders had the same name. Should I rename one of these folders just mention.

---
+++ it162/index.php
@@ -0,0 +1,24 @@
+<?php include 'includes/header.php';?>
+ <h2 class="pageID">ABOUT ME</h2>
+
+<!-- MAKE SURE YOU GET YOUR (3) IMAGES SAVED INTO YOUR IMAGES FOLDER -->
+ <img src="images/desktop.jpg" class="desktop" alt="photo of me" />
+ <img src="images/phone.jpg" class="phone" alt="photo of a puma" />
+ <p>I love to travel and watch the Seahawks. Also my goals is to grow and become a full stack developer that specialize in Python, database and Postgres plus understanding how to code or script using Visual Studio Code with Git and Github. Additionally, learning Google Cloud and Linux has help me to be more innovated and advanced in learning and understanding web development to hopefully one day be a great developer.</p>
+ <p>Connect with Will's codes</p>
+ <p></p>
+ <p></p>
+</section>
+<!-- END LEFT COL -->
+<!-- HEADER STARTS CUT ABOVE </section> -->
+
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Will Anderson codes</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="photo of a puma" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+ 
+<?php include 'includes/footer.php';?>
