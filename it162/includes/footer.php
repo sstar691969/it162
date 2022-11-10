63097ea3a6959d8fcbb3fd5162a1d7f63e1ff0fc
William L Anderson <91501606+sstar691969@users.noreply.github.com>
2022-11-09 16:59:02 -0800

    contact.php and includes folder commit
    finally done. update on contact page and email.

---
+++ it162/includes/footer.php
@@ -0,0 +1,34 @@
+ 
+<!-- START Footer HERE CUT -->
+<footer>
+      <p><small>&copy; 2018-<?=date("Y")?> by 
+          <a href="contact.php">William Anderson</a>, All Rights Reserved ~ 
+          <a id="html-checker" href="#">Check HTML</a> ~ 
+          <a id="css-checker" href="#">Check CSS</a></small>
+     </p>
+    </footer>
+<!-- END Footer --> 
+</main>
+<!-- END WRAPPER -->
+
+    
+
+
+
+  <script>
+	//https://tinyurl.com/dynamic-html-checker
+	document.getElementById("html-checker").setAttribute("href","https://validator.w3.org/nu/?doc=" + location.href);
+            document.getElementById("css-checker").setAttribute("href","https://jigsaw.w3.org/css-validator/validator?uri=" + location.href); 
+
+	//manages mobile nav - from W3Schools
+	function myFunction() {
+    	    var x = document.getElementById("myTopnav");
+    	    if (x.className === "topnav") {
+        	         x.className += " responsive";
+    	    } else {
+        	         x.className = "topnav";
+    	   }
+	}   
+ </script>
+</body>
+</html>
